5e5ce489c658ab2297c39993cb6d1075703af22d
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-05-31 23:08:51 -0500

    Hello World

---
+++ repaso_cero_a_basico--mouredev/HelloXcode/Shared/HelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeApp.swift
+//  Shared
+//
+//  Created by Tech Fitco on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

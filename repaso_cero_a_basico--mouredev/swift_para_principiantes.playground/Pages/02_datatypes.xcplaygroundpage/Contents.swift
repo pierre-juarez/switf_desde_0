c8786f0c0e30b44d8ac46319e0c8226af1a9da70
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-06-01 00:19:30 -0500

    Datatypes

--- repaso_cero_a_basico--mouredev/swift_para_principiantes.playground/Pages/02_datatypes.xcplaygroundpage/Contents.swift
+++ repaso_cero_a_basico--mouredev/swift_para_principiantes.playground/Pages/02_datatypes.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+//STRING
+let myString:String = "One text"
+let myString2 = "Other text"
+let myString3 = myString + " & " + myString2
+print(myString3)
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+//INT
+let int:Int = 1
+let int2 = 2
+let int3 = 2 + 1
+print("The sum is:", int3)
+
+
+//DOUBLE
+let double:Double = 2.5
+let double2 = 2.3
+let double3 = 1.0
+let sum = double + double2 + double3
+print("The sum of double's is:",sum)
+
+//FLOAT
+let float1:Float = 2.3
+let float2:Float = 2.5
+let sumf:Float = float1 + float2
+print("The sum of float's is:",sumf)
+
+//BOOL
+let bool1:Bool = true
+let bool2 = false
+print(bool1 && bool2)
+

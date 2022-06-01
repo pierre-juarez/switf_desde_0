59204abe184fb592bf7dc059439b4c9b2ca863be
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-05-31 23:46:06 -0500

    Variables and constants

---
+++ repaso_cero_a_basico--mouredev/swift_para_principiantes.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//Variables
+var myFirstVariable = "Hello world"
+print(myFirstVariable)
+myFirstVariable = "Other text"
+print(myFirstVariable)
+
+
+//No se puede asignar un tipo INT a uno de tipo STRING
+/* myFirstVariable = 1
+print(myFirstVariable) */
+
+
+//Constantes
+let myFirstConstant = "New constant"
+print(myFirstConstant)
+
+//Una constante no puede modificar su valor
+/* myFirstConstant = "Another constant"
+print(myFirstConstant) */

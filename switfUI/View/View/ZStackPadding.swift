6ee33918e0cba820c51bb1032034bb5df0d89efc
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-06-09 15:10:57 -0500

    Container modules

--- switfUI/View/View/ZStackPadding.swift
+++ switfUI/View/View/ZStackPadding.swift
@@ -8,49 +8,85 @@
 import SwiftUI
 
 struct ZStackPadding: View {
-  @State var curso : String = "iOs"
     var body: some View {
       ZStack{
         Color.blue
         
         VStack{
-          Image("platzi")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 100, height: 100, alignment: .center)
+          
           Text("Welcome to the course")
             .padding(.bottom,6.0)
           
-          ZStack{
-            
-            if curso == "" {
-              Text("Curso")
-                .foregroundColor(Color.red)
-            }
-            
-            TextField("", text: $curso)
-              .padding(.leading,8)
-          }
+          logos()
           
-          VStack (alignment: .trailing) {
-            Text("1").border(Color.blue)
-            Text("2").border(Color.blue)
-            Text("3").border(Color.blue)
-            
-            HStack (alignment: .top){
-              Text("A").frame(width: 100, height: 100, alignment: .center).border(Color.blue)
-              Text("B").border(Color.blue)
-              Text("C").border(Color.blue)
-            }.background(Color.green)
-            
-          }.border(Color.black)
-            .background(Color.red)
+          datosUsuario()
+         
+          reto()
+          
+          TextMod()
+          
           
         }
         
           
       }.ignoresSafeArea()
     }
+}
+
+struct logos: View{
+  var body: some View{
+    VStack{
+      Image("platzi")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 100, height: 100, alignment: .center)
+      
+      Image("platzi")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 100, height: 100, alignment: .center)
+      
+      Image("platzi")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 100, height: 100, alignment: .center)
+    }
+    }
+}
+
+struct datosUsuario: View{
+  @State var curso : String = "iOs"
+  var body: some View{
+    ZStack{
+      
+      if curso == "" {
+        Text("Curso")
+          .foregroundColor(Color.red)
+      }
+      
+      TextField("", text: $curso)
+        .padding(.leading,8)
+    }
+  }
+}
+
+struct reto: View{
+  var body: some View{
+    VStack (alignment: .trailing) {
+      Text("1").border(Color.blue)
+      Text("2").border(Color.blue)
+      Text("3").border(Color.blue)
+      
+      HStack (alignment: .top){
+        Text("A").frame(width: 100, height: 100, alignment: .center).border(Color.blue)
+        Text("B").border(Color.blue)
+        Text("C").border(Color.blue)
+      }.background(Color.green)
+      
+    }.border(Color.black)
+      .background(Color.red)
+    
+  }
 }
 
 struct ZStackPadding_Previews: PreviewProvider {

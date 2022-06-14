500b438e8991430973761d424d54673942abdd8c
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-06-14 00:37:39 -0500

    GameStream - RegisterView Complete , update LoginView

--- GameStreamAppiOs/GameStreamAppiOs/ContentView.swift
+++ GameStreamAppiOs/GameStreamAppiOs/ContentView.swift
@@ -64,7 +64,6 @@ struct LoginRegister: View{
         
         Spacer()
         
-        
       }
       
       Spacer(minLength: 42)
@@ -92,19 +91,19 @@ struct viewLogin: View{
       VStack (alignment:.leading){
         
         Text("Correo electrónico")
-          .foregroundColor(Color("PrimaryColor"))
+          .foregroundColor(Color("White"))
+          .fontWeight(.bold)
         
         ZStack(alignment: .leading){
           
           if email.isEmpty{
             Text(verbatim: "ejemplo@gmail.com")
-              .font(.caption)
               .foregroundColor(Color("WhiteAccent"))
+
           }
           
           
           TextField("", text: $email)
-            .font(.caption)
             .foregroundColor(Color("White"))
           
         }
@@ -116,18 +115,17 @@ struct viewLogin: View{
         
         Text("Contraseña")
           .foregroundColor(Color("White"))
+          .fontWeight(.bold)
         
         ZStack(alignment: .leading){
           
           if contraseña.isEmpty{
             Text(verbatim: "*******")
-              .font(.caption)
               .foregroundColor(Color("WhiteAccent"))
           }
           
           
           SecureField("", text: $contraseña)
-            .font(.caption)
             .foregroundColor(Color("White"))
           
         }
@@ -215,8 +213,151 @@ struct btnTwitter: View{
 }
 
 struct viewRegister: View{
+  
+  @State var email: String = ""
+  @State var contraseña: String = ""
+  @State var confirmarContraseña: String = ""
+  
   var body: some View{
-    Text("Register")
+  
+    
+    ScrollView{
+      
+      VStack (alignment:.center){
+        Text("Seleccione una foto de perfil")
+          .fontWeight(.bold)
+          .foregroundColor(Color("White"))
+          
+        
+        Text("Puedes cambiarla ahora o más adelante")
+          .font(.footnote)
+          .fontWeight(.light)
+          .foregroundColor(Color("WhiteAccent"))
+          .padding(.bottom)
+        
+        Button(action: uploadPhoto, label: {
+          ZStack {
+            Image("thumb_profile")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 80, height: 80)
+            
+            Image(systemName: "camera")
+              .foregroundColor(Color("White"))
+              
+          }
+        }).padding(.bottom)
+        
+      }
+      
+      VStack {
+        
+      
+        VStack(alignment:.leading){
+          
+          Text("Correo electrónico*")
+            .fontWeight(.bold)
+            .foregroundColor(Color("White"))
+          
+          ZStack(alignment: .leading){
+            
+            if email.isEmpty{
+              Text(verbatim: "ejemplo@gmail.com")
+                .foregroundColor(Color("WhiteAccent"))
+            }
+            
+            
+            TextField("", text: $email)
+              .foregroundColor(Color("White"))
+            
+          }
+          
+          Divider()
+            .frame(height:1)
+            .background(Color("PrimaryColor"))
+            .padding(.bottom)
+          
+          Text("Contraseña*")
+            .fontWeight(.bold)
+            .foregroundColor(Color("White"))
+          
+          ZStack(alignment: .leading){
+            
+            if contraseña.isEmpty{
+              Text(verbatim: "*******")
+                .foregroundColor(Color("WhiteAccent"))
+            }
+            
+            
+            SecureField("", text: $contraseña)
+              .foregroundColor(Color("White"))
+            
+          }
+          
+          Divider()
+            .frame(height:1)
+            .background(Color("PrimaryColor"))
+            .padding(.bottom)
+          
+          
+          Text("Confirmar contraseña*")
+            .fontWeight(.bold)
+            .foregroundColor(Color("White"))
+          
+          ZStack(alignment: .leading){
+            
+            if confirmarContraseña.isEmpty{
+              Text(verbatim: "*******")
+                .foregroundColor(Color("WhiteAccent"))
+            }
+            
+            
+            SecureField("", text: $confirmarContraseña)
+              .foregroundColor(Color("White"))
+            
+          }
+          
+          Divider()
+            .frame(height:1)
+            .background(Color("PrimaryColor"))
+            .padding(.bottom)
+          
+        }
+        
+        Button(action: registerUser, label: {
+          Text("REGÍSTRATE")
+            .fontWeight(.bold)
+            .foregroundColor(Color("White"))
+            .frame(maxWidth: .infinity,alignment: .center)
+            .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color("PrimaryColor"),lineWidth: 1).shadow(color: Color("White"), radius: 6))
+            .padding(.bottom)
+        })
+        
+        Text("Regístrate con redes sociales")
+          .foregroundColor(Color("White"))
+          .fontWeight(.thin)
+          .frame(width: 340, alignment: .center)
+          .padding(EdgeInsets(top: 20, leading: 0, bottom: 15, trailing: 0))
+        
+        HStack{
+          
+          Button(action: {print("Iniciando con Facebook...")}, label: {
+            btnFacebook()
+          })
+          
+          
+          Button(action: {print("Iniciando con Twitter...")}, label: {
+            btnTwitter()
+          })
+          
+        }
+        
+        
+      }
+      
+    }.padding(.horizontal,77)
+    
   }
 }
 
@@ -224,9 +365,17 @@ func login(){
   print("Iniciando sesión...")
 }
 
+func registerUser(){
+  print("Register user...")
+}
+
+func uploadPhoto(){
+  print("Uploading photo...")
+}
+
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
-    Image("LogIn")
+    Image("SignIn")
   }
 }

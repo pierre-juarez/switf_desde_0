fcc7a544a255bf9b1e4c038e72c5e6ed1bfeff67
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-06-15 22:59:19 -0500

    GameStream - Creating our Home screen

--- GameStreamAppiOs/GameStreamAppiOs/Home.swift
+++ GameStreamAppiOs/GameStreamAppiOs/Home.swift
@@ -8,41 +8,68 @@
 import SwiftUI
 
 struct Home: View {
+    @State var tabSelected:Int = 2
     var body: some View {
       
-      TabView{
+      
+      TabView (selection: $tabSelected){
         
         Text("Perfil")
           .font(.system(size: 30, weight: .bold, design: .rounded))
           .tabItem{
             Image(systemName: "person")
             Text("Perfil")
-          }
+          }.tag(0)
         
         Text("Juegos")
           .font(.system(size: 30, weight: .bold, design: .rounded))
           .tabItem{
             Image(systemName: "gamecontroller")
             Text("Juegos")
-          }
+          }.tag(1)
         
-        Text("Home")
-          .font(.system(size: 30, weight: .bold, design: .rounded))
+        ScreenHone()
           .tabItem{
             Image(systemName: "house")
             Text("Inicio")
-          }
+          }.tag(2)
         
         Text("Favoritos")
           .font(.system(size: 30, weight: .bold, design: .rounded))
           .tabItem{
             Image(systemName: "heart")
             Text("Favoritos")
-          }
+          }.tag(3)
         
       }
-      
+      .accentColor(Color("White"))
     }
+  
+  init(){
+    UITabBar.appearance().backgroundColor = UIColor(Color("TabBarColor"))
+    
+    print("Iniciando las vistas del Home...")
+    
+  }
+  
+}
+
+struct ScreenHone:View{
+  var body: some View{
+    
+    ZStack{
+      
+      Color("BackgroundColor").ignoresSafeArea()
+      
+      VStack{
+        Text("Homecito")
+          
+      }.padding(.horizontal,18)
+      
+    }.navigationBarHidden(true)
+      .navigationBarBackButtonHidden(true)
+    
+  }
 }
 
 struct Home_Previews: PreviewProvider {
